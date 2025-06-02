908ddf5a605a524c5eb28c7dfed833ee19e26e44
alex-dow <adow@psikon.com>
2025-06-01 22:07:44 -0400

    use NEXTAUTH_URL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.NEXTAUTH_URL,
     plugins: [
         usernameClient()
     ]
