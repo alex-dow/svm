1f4ffa8e97660ded3a850b322c8a4ffce95d2b31
alex-dow <adow@psikon.com>
2025-06-07 22:52:04 -0400

    show icons if items being used in platforms and stops int he accordion header

--- src/components/trains/platforms/PlatformAccordionHeader.tsx
+++ src/components/trains/platforms/PlatformAccordionHeader.tsx
@@ -2,6 +2,9 @@ import { TrainStationPlatform } from "@/server/db/schemas/trainStations";
 import { TogglePlatformModeButton } from "./buttons/TogglePlatformModeButton";
 import { MovePlatformButton } from "./buttons/MovePlatformButton";
 import { DeletePlatformButton } from "./buttons/DeletePlatformButton";
+import { handleGetPlatformItems } from "@/lib/actions/trainStations";
+import { items } from "@/lib/satisfactory/data";
+import Image from "next/image";
 
 export default async function PlatformAccordionHeader({
   platform,
@@ -11,18 +14,30 @@ export default async function PlatformAccordionHeader({
   totalPlatforms: number
   projectId: number
 }) {
+
+
+  const platformItems = await handleGetPlatformItems(platform.id);
+  
   return (
-    <div className="flex flex-1 justify-between items-center">
+    <div className="flex flex-1 items-center gap-4">
       <div>
         <div className="flex gap-1 items-center">
           <div className="text-xs text-gray-400">#{platform.id}</div>
           <h1>Platform #{platform.position}</h1>
-          
         </div>
+
         <span className="text-sm text-gray-400">Mode: { platform.mode === 'loading' ? 'Loading' : 'Unloading'}</span>
       </div>
-      
-      <div className="flex gap-1">
+      <div className="flex gap-1 flex-1">
+          {platformItems.map((item) => {
+            const itemData = items[item.item_id];
+
+
+            return (<Image src={"/data/items/" + itemData.icon + "_64.png"} key={item.item_id} alt={itemData.name} width={32} height={32}/>)
+          })}
+
+      </div>      
+      <div className="flex gap-1 justify-end">
         <TogglePlatformModeButton platformId={platform.id} mode={platform.mode}/>
         <MovePlatformButton disabled={platform.position == totalPlatforms} direction="down" platformId={platform.id} position={platform.position}/>
         <MovePlatformButton disabled={platform.position == 1} direction="up" platformId={platform.id} position={platform.position}/>
--- src/components/trains/timetable/StopAccordionTabHeader.tsx
+++ src/components/trains/timetable/StopAccordionTabHeader.tsx
@@ -1,6 +1,9 @@
 import { getTrainTimetableStop } from "@/lib/services/trains";
 import { MoveStopButton } from "./MoveStopButton";
 import DeleteStopButton from "../platforms/buttons/DeleteStopButton";
+import { handleGetStopItems } from "@/lib/actions/trains";
+import { items } from "@/lib/satisfactory/data";
+import Image from "next/image";
 
 export default async function StopAccordionTabHeader({
   stop,
@@ -10,7 +13,16 @@ export default async function StopAccordionTabHeader({
   totalStops: number
   projectId: number
 }) {
+
+
+
+  const stopItems = await handleGetStopItems(stop.id);
+
   return (
+
+
+
+
     <div className="flex flex-1 justify-between items-center">
       <div>
         <div className="flex gap-1 items-center">
@@ -18,6 +30,12 @@ export default async function StopAccordionTabHeader({
           <h1>#{stop?.position} #{stop?.station_name}</h1>
           
         </div>
+      </div>
+      <div className="flex flex-1">
+        {stopItems.map((item) => {
+          const itemData = items[item.item_id];
+          return (<Image src={"/data/items/" + itemData.icon + "_64.png"} key={item.item_id} alt={itemData.name} width={32} height={32}/>)
+        })}
       </div>
       
       <div className="flex gap-1">
