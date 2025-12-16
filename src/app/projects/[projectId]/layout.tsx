cd40427214a92c5adc4e513ec96255f7bf200b44
alex-dow <adow@psikon.com>
2025-12-16 00:35:06 -0500

    add a global confirmdialog box

--- src/app/projects/[projectId]/layout.tsx
+++ src/app/projects/[projectId]/layout.tsx
@@ -1,18 +1,24 @@
 
 import ItemsTabView from "@/components/projects/itemsTabs/ItemsTabView";
+import { ConfirmDialog } from "primereact/confirmdialog";
 
 
 export default async function ProjectLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: Promise<{ projectId: string }>;
 }) {
+  const { projectId } = await params;
   
 
   return (
     <div className="flex flex-1">
+      <ConfirmDialog />
       <div className="w-4/12 border-r border-stone-800">
-        <ItemsTabView />
+        <p>Project ID: { projectId }</p>
+        <ItemsTabView projectId={parseInt(projectId)} />
       </div>
       <div className="w-8/12">
         {children}
--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -3,12 +3,12 @@ import { getServerSession } from "@/lib/auth/server";
 import { getProject } from "@/lib/services/projects";
 
 export default async function ProjectPage({ params }: { params: Promise<{ projectId: string }> }) {
-        const { projectId } = await params;
-        const project = await getProjectAction(parseInt(projectId));
+  const { projectId } = await params;
+  const project = await getProjectAction(parseInt(projectId));
 
 
   return <div>
     <h1>{project.name}</h1>
-    <p>PagE: project id: { project.id }</p>
+    <p>Page: project id: { project.id }</p>
   </div>
 }
